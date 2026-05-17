d56dc6a7c41ee5e46081b2fb7294fe8153ffd2c1
RobertI321 <robert.israel2002@gmail.com>
2026-05-17 19:17:07 +0300

    mlflow

--- src/train.py
+++ src/train.py
@@ -2,12 +2,26 @@ from river import tree, metrics, drift
 from kafka import KafkaConsumer
 import json
 import mlflow
+from mlflow import MlflowClient
+import yaml
+import pickle
+import tempfile
+import os
 
-mlflow.set_tracking_uri("http://mlflow:5000")
-mlflow.set_experiment("experiment-1")
 
-GRACE_PERIOD = 200 # Model update period (number of events before model update)
-TRIGGER_FREQUENCY = GRACE_PERIOD  # 
+with open("config.yaml", "r") as f:
+    config = yaml.safe_load(f)
+
+model_config = config["model"]
+mlflow_config = config["mlflow"]
+
+mlflow.set_tracking_uri(config["mlflow"]["tracking_uri"])
+mlflow.set_experiment(config["mlflow"]["experiment_name"])
+client = MlflowClient()
+
+# Model update period (number of events before model update)
+GRACE_PERIOD = config["model"]["grace_period"]
+TRIGGER_FREQUENCY = GRACE_PERIOD  
 
 def extract_features_and_label(event):
     X = {
@@ -20,6 +34,35 @@ def extract_features_and_label(event):
     }
     y = event["label"]
     return X, y
+
+def save_model(model, run_id, model_name):
+    # figure out what the next version number will be
+
+    try:
+        client.create_registered_model(model_name)
+    except Exception:
+        pass  # already exists, ignore
+
+    try:
+        existing = client.search_model_versions(f"name='{model_name}'")
+        next_version = max([int(v.version) for v in existing], default=0) + 1 if existing else 1
+    except Exception:
+        next_version = 1
+    
+    artifact_path = f"model_v{next_version}"
+    
+    with tempfile.TemporaryDirectory() as tmp:
+        path = os.path.join(tmp, "model.pkl")
+        with open(path, "wb") as f:
+            pickle.dump(model, f)
+        mlflow.log_artifact(path, artifact_path=artifact_path)
+    
+    mv = client.create_model_version(
+        name=model_name,
+        source=f"runs:/{run_id}/{artifact_path}/model.pkl",
+        run_id=run_id
+    )
+    return mv.version
 
 
 def main():
@@ -34,45 +77,71 @@ def main():
         value_deserializer=lambda x: json.loads(x.decode('utf-8'))
     )
     
+
     # Models and metrics
-    model = tree.HoeffdingTreeClassifier(grace_period=GRACE_PERIOD)
+    model = tree.HoeffdingAdaptiveTreeClassifier(**model_config)
     metric = metrics.ROCAUC()  # Using AUC as the evaluation metric
 
     # Concept Drift Detection
     drift_detector  = drift.ADWIN()
-
-    try:
-        for message in consumer:
-            print(f'Received message from Kafka: KEY = {message.key}, PARTITION = {message.partition}, OFFSET = {message.offset}')
-            event = message.value
-            print(f"Received event: {event}")
-
-            # Get features and label
-            X, y = extract_features_and_label(event)
-
-            # Predict and update metric
-            y_pred = model.predict_proba_one(X)
-            metric.update(y, y_pred)
-            model.learn_one(X, y)
-            
-            # Update drift detector with the true label
-            drift_detector.update(y)
-
-            # Log AUC metric to MLflow
-            mlflow.log_metric("AUC", metric.get(), step=message.offset)
-
-            print(f"Processed event at timestamp {event['timestamp']}, AUC: {metric.get():.4f}, true label: {y}, predicted probabilities: {y_pred}")
-
-            # Check for concept drift
-            if drift_detector.change_detected:
-                print(f'Drift detected at offset {message.offset}')
-                mlflow.set_tag("drift_detected", "True")
-
-    except KeyboardInterrupt:
-        print("Stopping consumer...")
     
-    finally:
-        consumer.close()
+    with mlflow.start_run():
+        mlflow.log_param("model", "HoeffdingAdaptiveTreeClassifier")
+        mlflow.log_params(model_config)
+        mlflow.set_tag("stage", "Production") 
+
+        event_count = 0
+        try:
+            for message in consumer:
+                event_count += 1
+
+                print(f'Received message from Kafka: KEY = {message.key}, PARTITION = {message.partition}, OFFSET = {message.offset}')
+                event = message.value
+                print(f"Received event: {event}")
+
+                # Get features and label
+                X, y = extract_features_and_label(event)
+
+                # Predict and update metric
+                y_pred = model.predict_proba_one(X)
+                y_pred_class = model.predict_one(X)
+                metric.update(y, y_pred)
+                model.learn_one(X, y)
+                
+                # Update drift detector the error (1 if prediction is wrong, 0 if correct); detects when model starts making more errors than expected
+                error = 1 if y_pred_class != y else 0
+                drift_detector.update(error)
+
+                # Log AUC metric to MLflow
+                mlflow.log_metric("AUC", metric.get(), step=message.offset)
+
+                print(f"Processed event at timestamp {event['timestamp']}, AUC: {metric.get():.4f}, true label: {y}, predicted probabilities: {y_pred}, predicted class: {y_pred_class}")
+
+                # Check for concept drift
+                if drift_detector.drift_detected:
+                    print(f'Drift detected at offset {message.offset}')
+                    mlflow.set_tag(f"drift_at_event_{message.offset}", "True")
+                    mlflow.set_tag("total errors", drift_detector.n_detections)
+                
+                if event_count % TRIGGER_FREQUENCY == 0:
+                    print(f"Saving model version at event {event_count}")
+                    save_model(model, mlflow.active_run().info.run_id, mlflow_config["model_name"])
+
+                    versions = client.get_latest_versions(name=mlflow_config["model_name"])
+                    latest_version = max(versions, key=lambda v: int(v.version)).version
+                    client.set_registered_model_alias(
+                    name=mlflow_config["model_name"],
+                    version=latest_version,
+                    alias="Production",                    
+                    )
+                    print(f"Model version {latest_version} set as Production")
+
+
+        except KeyboardInterrupt:
+            print("Stopping consumer...")
+        
+        finally:
+            consumer.close()
 
 if __name__ == "__main__":
     main()
